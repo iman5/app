c7fe53df842d16bd3688dd95c376421484161bcd
Iman Janghorban <Iman.Janghorban5@gmail.com>
2024-12-08 21:09:51 -0500

    set customer profiles for mapping

---
+++ App.API/Mappings/CustomerProfile.cs
@@ -0,0 +1,26 @@
+﻿using App.Library.Models;
+using AutoMapper;
+
+namespace App.API.Mappings;
+
+/// <summary>
+/// Profile for mapping between Customer and CustomerDto.
+/// </summary>
+public class CustomerProfile : Profile
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CustomerProfile"/> class.
+    /// </summary>
+    public CustomerProfile()
+    {
+        /// <summary>
+        /// Maps the Customer entity to the CustomerDto.
+        /// </summary>
+        CreateMap<Customer, CustomerDto>();
+        
+        /// <summary>
+        /// Maps the CustomerDto to the Customer entity.
+        /// </summary>
+        CreateMap<CustomerDto, Customer>();
+    }
+}
