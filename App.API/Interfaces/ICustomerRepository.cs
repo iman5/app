b3f96aa83f04d5968a39d823a201f412fe78ba5a
Iman Janghorban <Iman.Janghorban5@gmail.com>
2024-12-08 21:01:55 -0500

    addedcustomer intefaces

---
+++ App.API/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using App.Library.Models;
+
+namespace App.API.Interfaces;
+
+public interface ICustomerRepository
+{    
+    Task<IEnumerable<Customer>> GetAllCustomersAsync();
+    Task<Customer> GetCustomerByIdAsync(Guid id);
+    Task AddCustomerAsync(Customer customer);
+    Task UpdateCustomerAsync(Customer customer);
+    Task DeleteCustomerAsync(Guid id);
+}
---
+++ App.API/Interfaces/ICustomerService.cs
@@ -0,0 +1,12 @@
+﻿using App.Library.Models;
+
+namespace App.API.Interfaces;
+
+public interface ICustomerService
+{
+    Task<IEnumerable<CustomerDto>> GetAllCustomersAsync();
+    Task<CustomerDto> GetCustomerByIdAsync(Guid id);
+    Task AddCustomerAsync(CustomerDto CustomerDto);
+    Task UpdateCustomerAsync(CustomerDto CustomerDto);
+    Task DeleteCustomerAsync(Guid id);
+}
