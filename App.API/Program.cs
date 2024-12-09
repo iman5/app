ef488f3e66d57a267e36e5c7a3424be2ce1017a5
Iman Janghorban <Iman.Janghorban5@gmail.com>
2024-12-08 21:18:08 -0500

    app services and pipeline is set

--- App.API/Program.cs
+++ App.API/Program.cs
@@ -1,9 +1,56 @@
+using App.API.Interfaces;
+using App.API.Repositories;
+using App.API.Services;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using App.API.Middlewares;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using AutoMapper;
+using App.API.Mappings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseInMemoryDatabase("DB"));
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>(); 
+builder.Services.AddScoped<CustomerService>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = new HeaderApiVersionReader("api-version");
+});
+var mapperConfig = new MapperConfiguration(map =>
+{
+    map.AddProfile<CustomerProfile>();
+});
+builder.Services.AddSingleton(mapperConfig.CreateMapper());
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(metrics =>
+    {
+        metrics.AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddRuntimeInstrumentation();//AddPrometheusExporter
+    })
+    .WithTracing(tracing =>
+    {
+        if (builder.Environment.IsDevelopment()) tracing.SetSampler(new AlwaysOnSampler());// We want to view all traces in development
+
+        tracing.AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation();//AddJaegerExporter
+    });
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext() 
+    .WriteTo.Console()
+    .CreateLogger();
+builder.Host.UseSerilog(Log.Logger);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,11 +62,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseSerilogRequestLogging();
+app.UseExceptionHandling();
+app.UseCors(a => a.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 app.MapControllers();
 
 app.Run();
+public partial class Program { }
+
+
+
+
+
+
