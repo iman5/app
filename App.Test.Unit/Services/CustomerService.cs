5f14b73f67ffba0dea914db15f76379171296865
Iman Janghorban <Iman.Janghorban5@gmail.com>
2024-12-08 21:46:56 -0500

    added customer service for unit testing

---
+++ App.Test.Unit/Services/CustomerService.cs
@@ -0,0 +1,38 @@
+﻿using App.Library.Models;
+
+namespace App.Test.Unit.Services;
+
+public class CustomerService : ICustomerService
+{
+    private readonly ICustomerService _customerService;
+
+    public CustomerService(ICustomerService customerService)
+    {
+        this._customerService = customerService;
+    }
+
+    public async Task<IEnumerable<CustomerDto>> GetAllCustomersAsync()
+    {
+        return await _customerService.GetAllCustomersAsync();
+    }
+
+    public async Task<CustomerDto> GetCustomerByIdAsync(Guid customerId)
+    {
+        return await _customerService.GetCustomerByIdAsync(customerId);
+    }
+
+    public async Task AddCustomerAsync(CustomerDto CustomerDto)
+    {
+        await _customerService.AddCustomerAsync(CustomerDto);
+    }
+
+    public async Task UpdateCustomerAsync(CustomerDto CustomerDto)
+    {
+        await _customerService.UpdateCustomerAsync(CustomerDto);
+    }
+
+    public async Task DeleteCustomerAsync(Guid customerId)
+    {
+        await _customerService.DeleteCustomerAsync(customerId);
+    }
+}
---
+++ App.Test.Unit/Services/ICustomerService.cs
@@ -0,0 +1,12 @@
+﻿using App.Library.Models;
+
+namespace App.Test.Unit.Services;
+
+public interface ICustomerService
+{
+    Task<IEnumerable<CustomerDto>> GetAllCustomersAsync();
+    Task<CustomerDto> GetCustomerByIdAsync(Guid id);
+    Task AddCustomerAsync(CustomerDto CustomerDto);
+    Task UpdateCustomerAsync(CustomerDto CustomerDto);
+    Task DeleteCustomerAsync(Guid id);
+}
