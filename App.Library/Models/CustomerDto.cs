38bb8d466a83230dff08c66f8a693afb15b15144
Iman Janghorban <Iman.Janghorban5@gmail.com>
2024-12-08 20:45:31 -0500

    Added library models

---
+++ App.Library/Models/CustomerDto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.Library.Models;
+
+public class CustomerDto {
+    [Display(Name = "Customer ID")]
+    public Guid CustomerId { get; set; } 
+
+    [Required(ErrorMessage = "First Name is required")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "First Name must be between 2 and 50 characters")]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; } = string.Empty;
+
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "First Name must be between 2 and 50 characters")]
+    [Display(Name = "Middle Name")]
+    public string? MiddleName { get; set; } 
+
+    [Required(ErrorMessage = "Last Name is required")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "First Name must be between 2 and 50 characters")]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email is required")] 
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    [Display(Name = "Email")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email is required")] 
+    [Phone(ErrorMessage = "Invalid Phone Number")]
+    [Display(Name = "Phone")]
+    public string Phone { get; set; } = string.Empty;
+}
